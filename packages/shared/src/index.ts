f41770e997232030748e2c551ff657d4586ac169
Frank <jssmith883@gmail.com>
2022-09-05 01:01:03 +0800

    Add more common function

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,16 @@
 export function isObject(value: any) {
     return typeof value === 'object' && value != null
 }
+
+export function isFunction(value: any) {
+    return typeof value === 'function'
+}
+
+export const isArray = Array.isArray
+
+export const isString = (value: any) => {
+    return typeof value === 'string'
+}
+
+export const assign = Object.assign
+
